1feefc5107b579f78d2016cec9a4306325071db8
chowanza <luiscandialesfajardo@gmail.com>
2024-01-23 00:44:18 -0400

    fullscreen button added

--- scripts/control.js
+++ scripts/control.js
@@ -6,6 +6,7 @@ const control = {
   resolve: document.getElementById('resolve'),
   reset: document.getElementById('reset'),
   hearts: document.querySelectorAll('.hearts'),
+  fullscreen: document.getElementById('fullscreen'),
 }
 
 control.reset.addEventListener('click', () => {
@@ -19,6 +20,24 @@ control.resolve.addEventListener('click', () => {
   deleteBoard()
   resolveSudoku()
 })
+
+let icon = control.fullscreen.querySelector('i');
+control.fullscreen.addEventListener('click', function() {
+  // Dentro de este evento, verificamos si el documento ya está en pantalla completa
+  if (!document.fullscreenElement) {
+    // Si no está en pantalla completa, solicitamos pantalla completa
+    document.documentElement.requestFullscreen();
+    // Cambiamos el ícono a 'fa-down-left-and-up-right-to-center'
+    icon.className = 'fa-solid fa-down-left-and-up-right-to-center';
+  } else {
+    // Si ya está en pantalla completa, salimos de la pantalla completa
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+      // Cambiamos el ícono de nuevo a 'fa-up-right-and-down-left-from-center'
+      icon.className = 'fa-solid fa-up-right-and-down-left-from-center';
+    }
+  }
+});
 
 let attempts = 2
 export const lose = () => {
